c5d6c9641d14acb3c28ffa1c04c82b87524b2858
Huu Chi Huynh <73843190+HuuChiHuynh@users.noreply.github.com>
2022-01-13 10:39:36 +0000

    Added delete entry

--- src/DeleteEntry.java
+++ src/DeleteEntry.java
@@ -3,25 +3,23 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class UpdateAuthor {
-    static final String DATABASE_URL = "jdbc:mysql://localhost/books";
+public class DeleteEntry {
+    final static String DATABASE_URL = "jdbc:mysql://localhost/books";
     public static void main(String[] args) {
         String firstName = "Lisa";
-        String lastName = "Brennan";
-
         Connection connection = null;
         PreparedStatement pstat = null;
 
-        int entriesUpdated = 0;
+        int entriesDeleted = 0;
         try {
             connection = DriverManager.getConnection(
-                    DATABASE_URL, "root", "password");
-            pstat = connection.prepareStatement("UPDATE Authors SET LASTNAME=? WHERE FirstName=?");
-            pstat.setString(1, lastName);
-            pstat.setString(2, firstName);
-            entriesUpdated = pstat.executeUpdate();
-            System.out.println(entriesUpdated + " records successfully updated into the database.");
+                    DATABASE_URL, "root", "password"
+            );
+            pstat = connection.prepareStatement("DELETE FROM Authors WHERE FirstName=?");
+            pstat.setString(1, firstName);
 
+            entriesDeleted = pstat.executeUpdate();
+            System.out.println(entriesDeleted + " records deleted from Database");
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
