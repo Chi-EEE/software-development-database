7ef3e38f757bc2c4e26a7645a9ed15ae1e90724f
Huu Chi Huynh <73843190+HuuChiHuynh@users.noreply.github.com>
2022-01-13 09:59:10 +0000

    ExecuteUpdate

---
+++ src/testDrive.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class testDrive {
+    public static void main(String[] args) {
+        final String DATABASE_URL = "jdbc:mysql://localhost/books";
+        Connection connection = null;
+        PreparedStatement pstat = null;
+        String firstName = "Lisa";
+        String lastName = "Smith";
+
+        int recordsAdded = 0;
+        try {
+            connection = DriverManager.getConnection(DATABASE_URL, "root", "password");
+
+            // make a statement to use for the database
+            pstat = connection.prepareStatement("INSERT INTO Authors (FirstName, LastNName) VALUES (?,?)");
+            pstat.setString(1, firstName);
+            pstat.setString(2, lastName);
+
+            recordsAdded = pstat.executeUpdate(); // When updating database then use executeUpdate else executeQuery
+            System.out.println(recordsAdded + " records successfully added into the database.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Attempt to close pstat and connection to prevent memory leak
+            try {
+                pstat.close();
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
